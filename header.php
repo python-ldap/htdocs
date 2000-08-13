68f52c6c8272eb30d2e094c5723769255c4b788d
leonard <leonard>
2000-08-13 22:47:29 +0000

    oops

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 
 	$release_root = "ftp://python-ldap.sourceforge.net/pub/python-ldap";
 
-	function release(name, desc, vers) {
+	function release($name, $desc, $vers) {
 		print "<tr><td><a href="$release_root/$name">$name</a></td>
 			   <td>$desc</td>
 			   <td>$vers</td></tr>\n";
