e03bdcd3d91708735c88f3148b36775c27ce7df5
leonard <leonard>
2000-08-14 08:47:03 +0000

    related documentation - mostly C stuff

--- docs.php
+++ docs.php
@@ -15,7 +15,18 @@
 Eventually there will be documentation links here, and maybe even a FAQ!
 </p>
 
+<h2>Related documentation</h2>
+<ul>
+<li><a href="http://developer.netscape.com/docs/manuals/dirsdk/csdk30">
+	Netscape Directory SDK 3.0 for C Programmer's Guide</a>
+<li><a href="http://www.ietf.org/rfc/rfc1823.txt">RFC1823: The LDAP 
+	Application Program Interface</a>
+<li><a href="http://www.ietf.org/internet-drafts/draft-ietf-ldapext-ldap-c-api-05.txt">The C LDAP Application Program Interface</a> (expired)
+<li><a href="http://www.openldap.org/software/man.cgi?query=ldap">OpenLDAP
+	manual pages</a>
+</ul>
+
 <hr>
-<i>$Id: docs.php,v 1.1 2000/08/14 08:17:34 leonard Exp $</i>
+<i>$Id: docs.php,v 1.2 2000/08/14 08:47:03 leonard Exp $</i>
 </BODY>
 </HTML>
